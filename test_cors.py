10fddaee07cc54095b9560dd89eed58ec4b586f9
root <root@frost-bridge.local>
2026-05-21 08:11:38 +0000

    housekeeping: cleanup unnecessary/test files, move to scratch, and bump version to 1.0.2

--- test_cors.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-res = requests.options("http://127.0.0.1:8000/api/devices/test", headers={
-    "Origin": "https://frosty.mwatney.com",
-    "Access-Control-Request-Method": "PATCH",
-    "Access-Control-Request-Headers": "authorization,content-type"
-})
-print("Status:", res.status_code)
-print("Headers:", res.headers)
--- test_cors_curl.sh
+++
@@ -1,4 +0,0 @@
-curl -v -X OPTIONS http://127.0.0.1:8000/api/devices/test \
-  -H "Origin: https://frosty.mwatney.com" \
-  -H "Access-Control-Request-Method: PATCH" \
-  -H "Access-Control-Request-Headers: authorization,content-type"
